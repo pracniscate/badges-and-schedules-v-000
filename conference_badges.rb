2393b9f7d485f6ce22f3e3291303b3f904a9803f
Lorena <lorena.giddens@outlook.com>
2018-06-05 12:26:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,10 @@
 def badge_maker(name)
-  print "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array_of_names)
   array_of_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  counter = 1
   array_of_names.each do |name|
-  puts "Hello, my name is #{array_of_names}."
-  counter += 1
+    "Hello, my name is #{array_of_names}."
   end
 end
