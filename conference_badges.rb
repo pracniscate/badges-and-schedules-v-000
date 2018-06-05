1424a0b801762bcc4f30300b84700127222ece2b
Lorena <lorena.giddens@outlook.com>
2018-06-05 12:16:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array_of_names)
+  array_of_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  array_of_names.each do |name|
+  puts "Hello, my name is #{array_of_names}."
+end
