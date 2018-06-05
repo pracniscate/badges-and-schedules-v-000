1023f85025155eb65216f857c2b82bf8c871cdbb
Lorena <lorena.giddens@outlook.com>
2018-06-05 12:31:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array_of_names)
-  array_of_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   array_of_names.collect{|name| badge_maker(name)}
 end
