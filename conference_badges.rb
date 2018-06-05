fa7d60697bf279341b54603045bb6c51a154dc62
Lorena <lorena.giddens@outlook.com>
2018-06-05 12:37:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,10 @@ end
 def batch_badge_creator(array_of_names)
   array_of_names.collect{|name| badge_maker(name)}
 end
+
+def assign_rooms(list_of_speakers)
+  assigned_rooms = []
+  list_of_speakers.each do |i|
+    assigned_rooms << "Hello, #{i}! You'll be assigned to room #{list_of_speakers.index(i) + 1}!"
+  end
+end
